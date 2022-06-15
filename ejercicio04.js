3dc492a7cc4f5ab8bdcf9778333631d3f3a6b1b1
jesusudiz <jesusgabrieludiz@gmail.com>
2022-06-14 22:48:28 -0400

    ejercicio realizado

--- ejercicio04.js
+++ ejercicio04.js
@@ -1,5 +1,3 @@
-
-
 //Elimina todos los valores falsos de una matriz.
 
 //Los valores falsos en JavaScript son false, null, 0, "", undefinedy NaN.
@@ -8,11 +6,11 @@
 
 
 function bouncer(arr) {
-    let newArray = [];
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i]) newArray.push(arr[i]);
-    }
-    return newArray;
+  let newArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i]) newArray.push(arr[i]);
   }
-  
-  bouncer([7, "ate", "", false, 9]);
+  return newArray;
+}
+
+bouncer([7, "ate", "", false, 9]);
